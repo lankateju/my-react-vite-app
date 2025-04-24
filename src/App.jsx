2f3f02d536a28c3f3c3426df7b81c8f9898cda7b
lankatejaswini <lanka.teju2000@gmail.com>
2025-04-24 13:18:02 +0530

    Initial commit for Vite React app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello from My First Vite React App! tejuuu👋</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
