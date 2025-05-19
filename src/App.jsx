c253543f607216c951a869ee574e7d17e034810f
lankatejaswini <lanka.teju2000@gmail.com>
2025-05-19 22:50:29 +0530

    Update App.jsx and changed the name teju

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello from My First Vite React App! tejuuuuuu👋</h1>
+      <h1>Hello from My First Vite React App! tejuuuuuuuuuuuuuuùuù👋</h1>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
